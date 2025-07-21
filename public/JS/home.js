617e79bd814c48c7b4d65af04e7658c173bc454a
Dawa Tamang <117086805+dawa-tmg@users.noreply.github.com>
2025-07-21 12:50:01 +0600

    Revert "some updates"

--- public/JS/home.js
+++ public/JS/home.js
@@ -114,132 +114,23 @@ function renderUser(id, fname, lname, email, account, number, pin, acType, balan
         sessionStorage.setItem('storeValue', storeValue)
         window.location.href = `./transactions.html`;
     });
-  userTable.classList.remove("hidden");
-});
 
-function renderUser(id, fname, lname, email, account, number, pin, acType, balance, date) {
-  const tableRow = document.createElement("tr");
-
-  const Id = document.createElement("td");
-  Id.setAttribute("class", "border border-2 p-2");
-  Id.innerText = id;
-
-  const fName = document.createElement("td");
-  fName.setAttribute("class", "border border-2 p-2");
-  fName.innerText = fname;
-
-  const lName = document.createElement("td");
-  lName.setAttribute("class", "border border-2 p-2");
-  lName.innerText = lname;
-
-  const mail = document.createElement("td");
-  mail.setAttribute("class", "border border-2 p-2");
-  mail.innerText = email;
-
-  const acc = document.createElement("td");
-  acc.setAttribute("class", "border border-2 p-2");
-  acc.innerText = account;
-
-  const phone = document.createElement("td");
-  phone.setAttribute("class", "border border-2 p-2");
-  phone.innerText = number;
-
-  const Pin = document.createElement("td");
-  Pin.setAttribute("class", "border border-2 p-2");
-  Pin.innerText = pin;
-
-  const accType = document.createElement("td");
-  accType.setAttribute("class", "border border-2 p-2");
-  accType.innerText = acType;
-
-  const Balance = document.createElement("td");
-  Balance.setAttribute("class", "border border-2 p-2");
-  Balance.innerText = balance;
-
-  const Date = document.createElement("td");
-  Date.setAttribute("class", "border border-2 p-2");
-  Date.innerText = date;
-
-  const actionTab = document.createElement("td");
-  actionTab.setAttribute("class", " p-2 text-center grid grid-cols-1 gap-2");
-
-  const TransactionBtn = document.createElement("button");
-  TransactionBtn.setAttribute("class", "bg-[#2e3a4d] rounded text-white py-1 px-3 buttonClick");
-  TransactionBtn.setAttribute("type", "submit");
-  TransactionBtn.innerText = "Transaction";
-
-  const UpdateBtn = document.createElement("button");
-  UpdateBtn.setAttribute("class", "bg-[#00b94b] rounded text-white py-1 px-3 buttonClick");
-  UpdateBtn.setAttribute("type", "submit");
-  UpdateBtn.innerText = "Update";
-
-  const DeleteBtn = document.createElement("button");
-  DeleteBtn.setAttribute("class", "bg-[#d5263e] rounded text-white py-1 px-3 buttonClick");
-  DeleteBtn.setAttribute("type", "submit");
-  DeleteBtn.innerText = "Delete";
-
-  TransactionBtn.addEventListener("click", () => {
-    const storeValue = id;
-    sessionStorage.setItem("storeValue", storeValue);
-    window.location.href = `./transactions.html`;
-  });
-
-  tableRow.appendChild(Id);
-  tableRow.appendChild(fName);
-  tableRow.appendChild(lName);
-  tableRow.appendChild(mail);
-  tableRow.appendChild(acc);
-  tableRow.appendChild(phone);
-  tableRow.appendChild(Pin);
-  tableRow.appendChild(accType);
-  tableRow.appendChild(Balance);
-  tableRow.appendChild(Date);
-  actionTab.appendChild(TransactionBtn);
-  actionTab.appendChild(UpdateBtn);
-  actionTab.appendChild(DeleteBtn);
-  tableRow.appendChild(actionTab);
-  userTable.appendChild(tableRow);
+    tableRow.appendChild(Id);
+    tableRow.appendChild(fName);
+    tableRow.appendChild(lName);
+    tableRow.appendChild(mail);
+    tableRow.appendChild(acc);
+    tableRow.appendChild(phone);
+    tableRow.appendChild(Pin);
+    tableRow.appendChild(accType);
+    tableRow.appendChild(Balance);
+    tableRow.appendChild(Date)
+    actionTab.appendChild(TransactionBtn);
+    actionTab.appendChild(UpdateBtn);
+    actionTab.appendChild(DeleteBtn);
+    tableRow.appendChild(actionTab);
+    userTable.appendChild(tableRow);
 }
-
-create.addEventListener("click", (event) => {
-  event.preventDefault();
-  actionButton.classList.add("hidden");
-  createAcc.classList.remove("hidden");
-  createAcc.classList.add("block");
-});
-
-send.addEventListener("click", (event) => {
-  event.preventDefault();
-  actionButton.classList.add("hidden");
-  transferMoney.classList.remove("hidden");
-  transferMoney.classList.add("block");
-});
-
-withdraw.addEventListener("click", (event) => {
-  event.preventDefault();
-  actionButton.classList.add("hidden");
-  withdrawMoney.classList.remove("hidden");
-  withdrawMoney.classList.add("block");
-});
-
-function createAccount() {
-  const formData = new FormData(createAcc);
-  const body = Object.fromEntries(formData);
-  axios
-    .post("http://localhost:5050/accounts", body)
-    .then((response) => {
-      alert("Account Created Successfully.");
-    })
-    .catch((error) => {
-      console.error("Error creating account:", error);
-      alert("Failed to create Account!!!");
-    });
-}
-
-createAcc.addEventListener("submit", (event) => {
-  event.preventDefault();
-  createAccount();
-});
 
 //Create Account Form Display
 create.addEventListener('click', (event)=>{
@@ -272,77 +163,14 @@ function createAccount(){
     axios.post('http://localhost:5050/accounts', body).then(response =>{
         alert('Account Created Successfully.');
     })
-  
-  //Transfer Money Submit Form function
-  function transferMoneyFunc() {
-  const formData = new FormData(transferMoney);
-  const body = Object.fromEntries(formData);
-  axios.post("http://localhost:5050/transfer", {
-      source_account: body.source_account,
-      destination_account: body.destination_account,
-      amount: Number(body.amount),
-    })
-    .then((response) => {
-      const transferMessage = document.createElement("div");
-      transferMoney.appendChild(transferMessage);
-      transferMessage.classList.remove("hidden");
-      transferMessage.classList.add(
-        "text-green-600",
-        "text-center",
-        "text-lg",
-        "font-semibold"
-      );
-      transferMessage.innerText = response.data.message;
-      transferMoney.reset();
-      alert("Transfer Successful.");
-    .catch((error) => {
-      const transferMessage = document.createElement("div");
-      transferMoney.appendChild(transferMessage);
-      transferMessage.classList.remove("hidden");
-      transferMessage.classList.add(
-        "text-red-600",
-        "text-center",
-        "text-lg",
-        "font-semibold"
-      );
-      transferMessage.innerText =
-        error.response?.data?.error || "Failed to transfer money.";
-      alert("Failed to transfer money.");
+    .catch(error => {
+        console.error('Error creating account:', error);
+        alert('Failed to create Account!!!');
     });
 }
 
-transferMoney.addEventListener("submit", (event) => {
-  event.preventDefault();
-  transferMoneyFunc();
-});
+createAcc.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    createAccount()
+})
 
-// Withdraw Money Submit Form function
-function withdrawMoneyFunc() {
-  const formData = new FormData(withdrawMoney);
-  const body = Object.fromEntries(formData);
-
-  axios
-    .post("http://localhost:5050/withdraw", {
-      source_account: body.source_account,
-      amount: Number(body.amount),
-    })
-    .then((response) => {
-      withdrawMessage.classList.remove("hidden");
-      withdrawMessage.classList.add("text-green-600");
-      withdrawMessage.innerText = response.data.message;
-      withdrawMoney.reset();
-      alert("Withdrawal Successful.");
-    })
-    .catch((error) => {
-      withdrawMessage.classList.remove("hidden");
-      withdrawMessage.classList.add("text-red-600");
-      withdrawMessage.innerText =
-        error.response?.data?.error || "Failed to withdraw money.";
-      alert("Failed to withdraw money.");
-    });
-}
-
-withdrawMoney.addEventListener("submit", (event) => {
-  event.preventDefault();
-  withdrawMoneyFunc();
-});
