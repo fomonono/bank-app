41cf938bea818489f510b817ededa01c5497d467
Chakar Pema Nyingpo <chakarpemanyingpo@Chakars-MacBook-Air.local>
2025-07-21 16:02:13 +0600

    adds updates on transactions and withdraw

--- public/JS/home.js
+++ public/JS/home.js
@@ -1,176 +1,274 @@
-const search = document.getElementById('search');
-const create = document.getElementById('create');
-const send = document.getElementById('send');
-const withdraw = document.getElementById('withdraw');
-const userTable = document.getElementById('userTable');
-const navigation = document.getElementById('navigation');
-const reset = document.getElementById('reset');
-const searchAcc = document.getElementById('searchAcc');
-const createAcc = document.getElementById('createAcc');
-const transferMoney = document.getElementById('transferMoney');
-const withdrawMoney = document.getElementById('withdrawMoney');
-const actionButton = document.getElementById('actionBtnContainer');
+const search = document.getElementById("search");
+const create = document.getElementById("create");
+const send = document.getElementById("send");
+const withdraw = document.getElementById("withdraw");
+const userTable = document.getElementById("userTable");
+const navigation = document.getElementById("navigation");
+const reset = document.getElementById("reset");
+const searchAcc = document.getElementById("searchAcc");
+const createAcc = document.getElementById("createAcc");
+const transferMoney = document.getElementById("transferMoney");
+const withdrawMoney = document.getElementById("withdrawMoney");
+const actionButton = document.getElementById("actionBtnContainer");
 
 //Search User by Account Number
-search.addEventListener('click', (event)=>{
-    event.preventDefault()
-    actionButton.classList.add('hidden')
-    searchAcc.classList.remove('hidden')
-    searchAcc.classList.add('block');
-})
-
-searchAcc.addEventListener('submit', function(event) {
-    event.preventDefault();
-    const inputValue = document.getElementById("accountSearch").value;
-    axios.get(`http://localhost:5050/accounts/number/${inputValue}`).then((response)=>{
-        console.log(response)
-        response.data.forEach((user)=>{
-            renderUser(
-                user.id, 
-                user.first_name,
-                user.last_name,
-                user.email,
-                user.account_number, 
-                user.phone_number,
-                user.pin_code, 
-                user.account_type,
-                user.balance,
-                user.created_at
-            )
-        })
-    })
-    userTable.classList.remove('hidden')
-    navigation.classList.remove('hidden')
-    searchAcc.reset()
+search.addEventListener("click", (event) => {
+  event.preventDefault();
+  actionButton.classList.add("hidden");
+  searchAcc.classList.remove("hidden");
+  searchAcc.classList.add("block");
 });
 
-reset.addEventListener('click', ()=>{
-    location.reload()
-})
-
-function renderUser(id, fname, lname, email, account, number, pin, acType, balance, date){
-    const tableRow = document.createElement('tr');
-
-    const Id = document.createElement('td');
-    Id.setAttribute('class', 'border border-2 p-2');
-    Id.innerText = id;
-
-    const fName = document.createElement('td');
-    fName.setAttribute('class', 'border border-2 p-2');
-    fName.innerText = fname;
-
-    const lName = document.createElement('td');
-    lName.setAttribute('class', 'border border-2 p-2');
-    lName.innerText = lname;
-
-    const mail = document.createElement('td');
-    mail.setAttribute('class', 'border border-2 p-2');
-    mail.innerText = email;
-
-    const acc = document.createElement('td');
-    acc.setAttribute('class', 'border border-2 p-2');
-    acc.innerText = account;
-
-    const phone = document.createElement('td');
-    phone.setAttribute('class', 'border border-2 p-2');
-    phone.innerText = number;
-
-    const Pin = document.createElement('td');
-    Pin.setAttribute('class', 'border border-2 p-2');
-    Pin.innerText = pin;
-
-    const accType = document.createElement('td');
-    accType.setAttribute('class', 'border border-2 p-2');
-    accType.innerText = acType;
-
-    const Balance = document.createElement('td');
-    Balance.setAttribute('class', 'border border-2 p-2');
-    Balance.innerText = balance;
-
-    const Date = document.createElement('td');
-    Date.setAttribute('class', 'border border-2 p-2');
-    Date.innerText = date;
-
-    const actionTab = document.createElement('td');
-    actionTab.setAttribute('class', ' p-2 text-center grid grid-cols-1 gap-2');
-    
-    const TransactionBtn = document.createElement('button');
-    TransactionBtn.setAttribute('class', 'bg-[#2e3a4d] rounded text-white py-1 px-3 buttonClick');
-    TransactionBtn.setAttribute('type', 'submit');
-    TransactionBtn.innerText = 'Transaction';
-
-    const UpdateBtn = document.createElement('button');
-    UpdateBtn.setAttribute('class', 'bg-[#00b94b] rounded text-white py-1 px-3 buttonClick');
-    UpdateBtn.setAttribute('type', 'submit');
-    UpdateBtn.innerText = 'Update';
-
-    const DeleteBtn = document.createElement('button');
-    DeleteBtn.setAttribute('class', 'bg-[#d5263e] rounded text-white py-1 px-3 buttonClick');
-    DeleteBtn.setAttribute('type', 'submit');
-    DeleteBtn.innerText = 'Delete';
-
-    TransactionBtn.addEventListener('click', () => {
-        const storeValue = id;
-        sessionStorage.setItem('storeValue', storeValue)
-        window.location.href = `./transactions.html`;
+searchAcc.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const inputValue = document.getElementById("accountSearch").value;
+  axios
+    .get(`http://localhost:5050/accounts/number/${inputValue}`)
+    .then((response) => {
+      console.log(response);
+      response.data.forEach((user) => {
+        renderUser(
+          user.id,
+          user.first_name,
+          user.last_name,
+          user.email,
+          user.account_number,
+          user.phone_number,
+          user.pin_code,
+          user.account_type,
+          user.balance,
+          user.created_at
+        );
+      });
     });
+  userTable.classList.remove("hidden");
+  navigation.classList.remove("hidden");
+  searchAcc.reset();
+});
 
-    tableRow.appendChild(Id);
-    tableRow.appendChild(fName);
-    tableRow.appendChild(lName);
-    tableRow.appendChild(mail);
-    tableRow.appendChild(acc);
-    tableRow.appendChild(phone);
-    tableRow.appendChild(Pin);
-    tableRow.appendChild(accType);
-    tableRow.appendChild(Balance);
-    tableRow.appendChild(Date)
-    actionTab.appendChild(TransactionBtn);
-    actionTab.appendChild(UpdateBtn);
-    actionTab.appendChild(DeleteBtn);
-    tableRow.appendChild(actionTab);
-    userTable.appendChild(tableRow);
+reset.addEventListener("click", () => {
+  location.reload();
+});
+
+function renderUser(
+  id,
+  fname,
+  lname,
+  email,
+  account,
+  number,
+  pin,
+  acType,
+  balance,
+  date
+) {
+  const tableRow = document.createElement("tr");
+
+  const Id = document.createElement("td");
+  Id.setAttribute("class", "border border-2 p-2");
+  Id.innerText = id;
+
+  const fName = document.createElement("td");
+  fName.setAttribute("class", "border border-2 p-2");
+  fName.innerText = fname;
+
+  const lName = document.createElement("td");
+  lName.setAttribute("class", "border border-2 p-2");
+  lName.innerText = lname;
+
+  const mail = document.createElement("td");
+  mail.setAttribute("class", "border border-2 p-2");
+  mail.innerText = email;
+
+  const acc = document.createElement("td");
+  acc.setAttribute("class", "border border-2 p-2");
+  acc.innerText = account;
+
+  const phone = document.createElement("td");
+  phone.setAttribute("class", "border border-2 p-2");
+  phone.innerText = number;
+
+  const Pin = document.createElement("td");
+  Pin.setAttribute("class", "border border-2 p-2");
+  Pin.innerText = pin;
+
+  const accType = document.createElement("td");
+  accType.setAttribute("class", "border border-2 p-2");
+  accType.innerText = acType;
+
+  const Balance = document.createElement("td");
+  Balance.setAttribute("class", "border border-2 p-2");
+  Balance.innerText = balance;
+
+  const Date = document.createElement("td");
+  Date.setAttribute("class", "border border-2 p-2");
+  Date.innerText = date;
+
+  const actionTab = document.createElement("td");
+  actionTab.setAttribute("class", " p-2 text-center grid grid-cols-1 gap-2");
+
+  const TransactionBtn = document.createElement("button");
+  TransactionBtn.setAttribute(
+    "class",
+    "bg-[#2e3a4d] rounded text-white py-1 px-3 buttonClick"
+  );
+  TransactionBtn.setAttribute("type", "submit");
+  TransactionBtn.innerText = "Transaction";
+
+  const UpdateBtn = document.createElement("button");
+  UpdateBtn.setAttribute(
+    "class",
+    "bg-[#00b94b] rounded text-white py-1 px-3 buttonClick"
+  );
+  UpdateBtn.setAttribute("type", "submit");
+  UpdateBtn.innerText = "Update";
+
+  const DeleteBtn = document.createElement("button");
+  DeleteBtn.setAttribute(
+    "class",
+    "bg-[#d5263e] rounded text-white py-1 px-3 buttonClick"
+  );
+  DeleteBtn.setAttribute("type", "submit");
+  DeleteBtn.innerText = "Delete";
+
+  TransactionBtn.addEventListener("click", () => {
+    const storeValue = id;
+    sessionStorage.setItem("storeValue", storeValue);
+    window.location.href = `./transactions.html`;
+  });
+
+  tableRow.appendChild(Id);
+  tableRow.appendChild(fName);
+  tableRow.appendChild(lName);
+  tableRow.appendChild(mail);
+  tableRow.appendChild(acc);
+  tableRow.appendChild(phone);
+  tableRow.appendChild(Pin);
+  tableRow.appendChild(accType);
+  tableRow.appendChild(Balance);
+  tableRow.appendChild(Date);
+  actionTab.appendChild(TransactionBtn);
+  actionTab.appendChild(UpdateBtn);
+  actionTab.appendChild(DeleteBtn);
+  tableRow.appendChild(actionTab);
+  userTable.appendChild(tableRow);
 }
 
 //Create Account Form Display
-create.addEventListener('click', (event)=>{
-    event.preventDefault()
-    actionButton.classList.add('hidden')
-    createAcc.classList.remove('hidden')
-    createAcc.classList.add('block');
-})
+create.addEventListener("click", (event) => {
+  event.preventDefault();
+  actionButton.classList.add("hidden");
+  createAcc.classList.remove("hidden");
+  createAcc.classList.add("block");
+});
 
 //Transfer Money Form Display
-send.addEventListener('click', (event)=>{
-    event.preventDefault()
-    actionButton.classList.add('hidden')
-    transferMoney.classList.remove('hidden')
-    transferMoney.classList.add('block');
-})
+send.addEventListener("click", (event) => {
+  event.preventDefault();
+  actionButton.classList.add("hidden");
+  transferMoney.classList.remove("hidden");
+  transferMoney.classList.add("block");
+});
 
 //Withdraw Money Form Display
-withdraw.addEventListener('click', (event)=>{
-    event.preventDefault()
-    actionButton.classList.add('hidden')
-    withdrawMoney.classList.remove('hidden')
-    withdrawMoney.classList.add('block');
-})
+withdraw.addEventListener("click", (event) => {
+  event.preventDefault();
+  actionButton.classList.add("hidden");
+  withdrawMoney.classList.remove("hidden");
+  withdrawMoney.classList.add("block");
+});
 
 //Create Account Form Submmit function
-function createAccount(){
-    const formData = new FormData(createAcc);
-    const body = Object.fromEntries(formData);
-    axios.post('http://localhost:5050/accounts', body).then(response =>{
-        alert('Account Created Successfully.');
+function createAccount() {
+  const formData = new FormData(createAcc);
+  const body = Object.fromEntries(formData);
+  axios
+    .post("http://localhost:5050/accounts", body)
+    .then((response) => {
+      alert("Account Created Successfully.");
     })
-    .catch(error => {
-        console.error('Error creating account:', error);
-        alert('Failed to create Account!!!');
+    .catch((error) => {
+      console.error("Error creating account:", error);
+      alert("Failed to create Account!!!");
     });
 }
 
-createAcc.addEventListener('submit', (event)=>{
-    event.preventDefault()
-    createAccount()
-})
+createAcc.addEventListener("submit", (event) => {
+  event.preventDefault();
+  createAccount();
+});
 
+function transferMoneyFunc() {
+  const formData = new FormData(transferMoney);
+  const body = Object.fromEntries(formData);
+
+  axios
+    .post("http://localhost:5050/transfer", {
+      source_account: body.source_account,
+      destination_account: body.destination_account,
+      amount: Number(body.amount),
+    })
+    .then((response) => {
+      const transferMessage = document.createElement("div");
+      transferMoney.appendChild(transferMessage);
+      transferMessage.classList.remove("hidden");
+      transferMessage.classList.add(
+        "text-green-600",
+        "text-center",
+        "text-lg",
+        "font-semibold"
+      );
+      transferMessage.innerText = response.data.message;
+      transferMoney.reset();
+      alert("Transfer Successful.");
+    })
+    .catch((error) => {
+      const transferMessage = document.createElement("div");
+      transferMoney.appendChild(transferMessage);
+      transferMessage.classList.remove("hidden");
+      transferMessage.classList.add(
+        "text-red-600",
+        "text-center",
+        "text-lg",
+        "font-semibold"
+      );
+      transferMessage.innerText =
+        error.response?.data?.error || "Failed to transfer money.";
+      alert("Failed to transfer money.");
+    });
+}
+
+transferMoney.addEventListener("submit", (event) => {
+  event.preventDefault();
+  transferMoneyFunc();
+});
+
+function withdrawMoneyFunc() {
+  const formData = new FormData(withdrawMoney);
+  const body = Object.fromEntries(formData);
+
+  axios
+    .post("http://localhost:5050/withdraw", {
+      source_account: body.source_account,
+      amount: Number(body.amount),
+    })
+    .then((response) => {
+      withdrawMessage.classList.remove("hidden");
+      withdrawMessage.classList.add("text-green-600");
+      withdrawMessage.innerText = response.data.message;
+      withdrawMoney.reset();
+      alert("Withdrawal Successful.");
+    })
+    .catch((error) => {
+      withdrawMessage.classList.remove("hidden");
+      withdrawMessage.classList.add("text-red-600");
+      withdrawMessage.innerText =
+        error.response?.data?.error || "Failed to withdraw money.";
+      alert("Failed to withdraw money.");
+    });
+}
+
+withdrawMoney.addEventListener("submit", (event) => {
+  event.preventDefault();
+  withdrawMoneyFunc();
+});
--- server/server.js
+++ server/server.js
@@ -1,68 +1,192 @@
-const express = require('express');
-const cors = require('cors');
-const knex = require('knex');
-const knexConfig = require('../knexfile')
+const express = require("express");
+const cors = require("cors");
+const knex = require("knex");
+const knexConfig = require("../knexfile");
 
 const app = express();
 const port = 5050;
 
 //DB connection
-const kn = knex(knexConfig.development)
+const kn = knex(knexConfig.development);
 
 app.use(cors());
 app.use(express.json());
 
 //Server check
-app.get('/greetings', (req, res)=>{
-  res.send('Hey There!!!')
-})
+app.get("/greetings", (req, res) => {
+  res.send("Hey There!!!");
+});
 
 //Get all accounts
-app.get('/accounts', async(req, res)=>{
-  const data = await kn('accounts').select('*');
+app.get("/accounts", async (req, res) => {
+  const data = await kn("accounts").select("*");
   res.json(data);
-})
+});
 
 //Get account by ID
-app.get('/accounts/:id', async(req, res)=>{
+app.get("/accounts/:id", async (req, res) => {
   const { id } = req.params;
-  const data = await kn('accounts').where('id', id);
+  const data = await kn("accounts").where("id", id);
   res.send(data);
-})
+});
 
 //Get account by Account Number
-app.get('/accounts/number/:account_number', async(req, res)=>{
+app.get("/accounts/number/:account_number", async (req, res) => {
   const { account_number } = req.params;
-  const data = await kn('accounts').where('account_number', account_number);
+  const data = await kn("accounts").where("account_number", account_number);
   res.send(data);
-})
+});
 
 //Get all Transactions
-app.get('/transactions', async(req, res)=>{
-  const data = await kn('transactions').select('*');
+app.get("/transactions", async (req, res) => {
+  const data = await kn("transactions").select("*");
   res.send(data);
-})
+});
 
 //Get transactions by ID
-app.get('/transactions/:id', async(req, res)=>{
+app.get("/transactions/:id", async (req, res) => {
   const { id } = req.params;
-  const data = await kn('transactions').where('id', id);
+  const data = await kn("transactions").where("id", id);
   res.send(data);
-})
+});
 
 //Post new user to account
-app.post('/accounts', async(req, res)=>{
-  try{
-    const { first_name, last_name, email, account_number, phone_number, pin_code, account_type, balance } = req.body;
-    await kn('accounts').insert({ first_name, last_name, email, account_number, phone_number, pin_code, account_type, balance });
-    return res.status(201).json({ message: 'Account Created Successfully' });
-  }catch (err) {
-    console.error('Database insert error:', err);
-    res.status(500).json({ error: 'Failed to create account' });
+app.post("/accounts", async (req, res) => {
+  try {
+    const {
+      first_name,
+      last_name,
+      email,
+      account_number,
+      phone_number,
+      pin_code,
+      account_type,
+      balance,
+    } = req.body;
+    await kn("accounts").insert({
+      first_name,
+      last_name,
+      email,
+      account_number,
+      phone_number,
+      pin_code,
+      account_type,
+      balance,
+    });
+    return res.status(201).json({ message: "Account Created Successfully" });
+  } catch (err) {
+    console.error("Database insert error:", err);
+    res.status(500).json({ error: "Failed to create account" });
   }
-})
+});
+
+app.post("/withdraw", async (req, res) => {
+  const { source_account, amount } = req.body;
+  const withdrawalAmount = Number(amount);
+
+  try {
+    const result = await kn.transaction(async (trx) => {
+      const account = await trx("accounts")
+        .where("account_number", source_account)
+        .first();
+
+      await trx("accounts")
+        .where("account_number", source_account)
+        .update({
+          balance: account.balance - withdrawalAmount,
+        });
+
+      await trx("withdraw").insert({
+        source_account,
+        amount: withdrawalAmount,
+        created_at: kn.fn.now(),
+      });
+
+      return { message: "Withdrawal successful!" };
+    });
+
+    return res.status(200).json(result);
+  } catch (err) {
+    return res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.post("/transfer", async (req, res) => {
+  const { source_account, destination_account, amount } = req.body;
+  const transferAmount = Number(amount);
+
+  try {
+    const result = await kn.transaction(async (trx) => {
+      const sourceAccount = await trx("accounts")
+        .where("account_number", source_account)
+        .first();
+
+      const destAccount = await trx("accounts")
+        .where("account_number", destination_account)
+        .first();
+
+      await trx("accounts")
+        .where("account_number", source_account)
+        .update({
+          balance: sourceAccount.balance - transferAmount,
+        });
+
+      await trx("accounts")
+        .where("account_number", destination_account)
+        .update({
+          balance: destAccount.balance + transferAmount,
+        });
+
+      await trx("transactions").insert({
+        balance: sourceAccount.balance - transferAmount,
+        debited: transferAmount,
+        credited: transferAmount,
+        recived_from: sourceAccount.id,
+        transfered_to: destAccount.id,
+        transaction_date: kn.fn.now(),
+      });
+
+      return { message: "Transfer successful!" };
+    });
+
+    return res.status(200).json(result);
+  } catch (err) {
+    console.error("Transfer error:", err);
+    return res
+      .status(500)
+      .json({ error: err.message || "Something went wrong" });
+  }
+});
+
+app.get("/transfers/:account_number", async (req, res) => {
+  const { account_number } = req.params;
+
+  try {
+    const account = await kn("accounts")
+      .where("account_number", account_number)
+      .first();
+
+    const transfers = await kn("transactions")
+      .select(
+        "transaction_date",
+        "debited",
+        "credited",
+        kn.raw(
+          "CASE WHEN recived_from = ? THEN 'outgoing' ELSE 'incoming' END as type",
+          [account.id]
+        )
+      )
+      .where("recived_from", account.id)
+      .orWhere("transfered_to", account.id)
+      .orderBy("transaction_date", "desc");
+
+    return res.status(200).json(transfers);
+  } catch (err) {
+    return res.status(500).json({ error: "Something went wrong" });
+  }
+});
 
 //Server listening to the defined port
-app.listen(port, ()=>{
-  console.log(`I am listening to ${port}`)
-})
+app.listen(port, () => {
+  console.log(`I am listening to ${port}`);
+});
